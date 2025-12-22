231beab76803018de6b9095740b6d2163c11e582
waleedkhano <khanow99@gmail.com>
2025-12-23 02:34:30 +0500

    added update flight api

--- src/controllers/flight_controller.js
+++ src/controllers/flight_controller.js
@@ -53,7 +53,49 @@ const getAll = async (req, res) => {
   }
 };
 
+const get = async (req, res) => {
+  try {
+    const flight = await flightService.getFlight(req.params.id);
+    return res.status(SuccessCodes.OK).json({
+      data: flight,
+      success: true,
+      message: "Successfully fetched the flight",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to fetch the flight",
+      err: error,
+    });
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const flight = await flightService.updateFlight(req.params.id, req.body);
+    return res.status(SuccessCodes.OK).json({
+      data: flight,
+      success: true,
+      message: "Successfully updated the flight",
+      err: {},
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      data: {},
+      success: false,
+      message: "Not able to update the flight",
+      err: error,
+    });
+  }
+}
+
 module.exports = {
   create,
   getAll,
+  get,
+  update
 };
--- src/repository/flight-repo.js
+++ src/repository/flight-repo.js
@@ -61,6 +61,22 @@ class FlightRepository {
       throw { error };
     }
   }
+
+  async updateFlight(flightId, data) {
+    try {
+      await Flights.update(data, {
+        where: {
+          id: flightId,
+        },
+      });
+      return  true;
+    }
+    catch (error) {
+      console.log("Something went wrong in the repository layer");
+      throw { error };
+    }
+  }
+
 }
 
 module.exports = FlightRepository;
